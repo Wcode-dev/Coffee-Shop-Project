cb04ce00d0c2d34d335579ebc0e64bec089ff35d
Wcode <134259109+Wcode-dev@users.noreply.github.com>
2023-12-14 17:47:38 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// ---------------------------Scroll-Header------------------------
+let header = document.querySelector('header');
+window.addEventListener('scroll' , () =>{
+    header.classList.toggle('shadow' , window.scrollY > 0 );
+});
+
+// ---------------------------Show-Menu------------------------
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active')
+}
+window.onscroll = () => {
+    navbar.classList.remove('active')
+}
